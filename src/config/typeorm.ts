d11c469fa8dbc2d394ddd41b5d3453832db6dd10
kssampson <kylesampsonmusic@gmail.com>
2024-10-18 14:56:30 -0700

    including radix

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -7,7 +7,7 @@ dotenvConfig();
 const config = {
     type: 'postgres',
     host: `${process.env.DATABASE_HOST}`,
-    port: parseInt(`${process.env.DATABASE_PORT}`),
+    port: parseInt(`${process.env.DATABASE_PORT}`, 10),
     username: `${process.env.DATABASE_USERNAME}`,
     password: `${process.env.DATABASE_PASSWORD}`,
     database: `${process.env.DATABASE_NAME}`,
